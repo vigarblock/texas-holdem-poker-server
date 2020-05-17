6ceffa5f6987e65cddc457c3865bcfd400de86c8
Vigar Block <vigardev@outlook.com>
2020-05-17 08:57:03 +0800

    Add unit tests for win determiner

---
+++ src/constants/texasHoldemRankings.js
@@ -0,0 +1,14 @@
+const texasHoldemRankings = {
+    1 : 'Royal Flush',
+    2 : 'Straight Flush',
+    3 : 'Four of a Kind',
+    4 : 'Full house',
+    5 : 'Flush',
+    6 : 'Straight',
+    7 : 'Three of a Kind',
+    8 : 'Two Pair',
+    9 : 'One Pair',
+    10 : 'High Card',
+}
+
+module.exports = texasHoldemRankings;
--- tests/texas-holdem/winDeterminer.js
+++ tests/texas-holdem/winDeterminer.js
@@ -1,6 +1,76 @@
-var assert = require("assert");
-var winDeterminer = require("../../src/texas-holdem/winDeterminer");
+const assert = require("assert");
+const winDeterminer = require("../../src/texas-holdem/winDeterminer");
+const texasHoldemRankings = require("../../src/constants/texasHoldemRankings");
 
-describe("Win Determiner", () => {
-    // TODO:
+
+describe.only("Win Determiner", () => {
+  it("Should return winning player when one player has higher rank than the rest", () => {
+    // Arrange
+    const communityCards = [
+      { suit: "Heart", value: "3" },
+      { suit: "Heart", value: "K" },
+      { suit: "Spade", value: "Q" },
+      { suit: "Diamond", value: "5" },
+      { suit: "Heart", value: "7" },
+    ];
+
+    const players = [
+      {
+        id: "foo",
+        playerHand: [
+          { suit: "Spade", value: "J" },
+          { suit: "Heart", value: "Q" },
+        ],
+      },
+      {
+        id: "bar",
+        playerHand: [
+          { suit: "Club", value: "K" },
+          { suit: "Heart", value: "K" },
+        ],
+      },
+    ];
+
+    // Act
+    const result = winDeterminer.getWinner(players, communityCards);
+
+    // Assert
+    assert.equal(result.winner.id, "bar");
+    assert.equal(result.winningRankMessage, texasHoldemRankings[7]);
+  });
+
+  it("Should return winning player when all players have the same rank", () => {
+    // Arrange
+    const communityCards = [
+      { suit: "Heart", value: "A" },
+      { suit: "Heart", value: "5" },
+      { suit: "Spade", value: "A" },
+      { suit: "Diamond", value: "Q" },
+      { suit: "Heart", value: "3" },
+    ];
+
+    const players = [
+      {
+        id: "foo",
+        playerHand: [
+          { suit: "Spade", value: "Q" },
+          { suit: "Heart", value: "J" },
+        ],
+      },
+      {
+        id: "bar",
+        playerHand: [
+          { suit: "Club", value: "Q" },
+          { suit: "Heart", value: "4" },
+        ],
+      },
+    ];
+
+    // Act
+    const result = winDeterminer.getWinner(players, communityCards);
+
+    // Assert
+    // assert.equal(result.winner.id, "bar");
+    // assert.equal(result.winningRankMessage, texasHoldemRankings[7]);
+  });
 });
