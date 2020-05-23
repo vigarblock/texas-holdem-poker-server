a5fdf526b2a801eecb475028605375b0cbc0db98
Vigar Block <vigardev@outlook.com>
2020-05-23 18:33:22 +0800

    Enable cors

--- router.js
+++ router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const GameManager = require("./src/gameManager");
 const router = express.Router();
+const cors = require('cors');
 
 router.get("/", (req, res) => {
   res.send("Welcome to texas holdem server");
 });
 
-router.get("/create-game", (req, res) => {
+router.get("/create-game", cors(), (req, res) => {
   const gameId = GameManager.createGame();
   res.json({ gameId });
 });
