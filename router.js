04a7ff531d0893c63e3e6bedb26bfe94b6d0d2b4
Vigar Block <vigardev@outlook.com>
2020-06-14 11:22:08 +0800

    Remove admin toggle

--- router.js
+++ router.js
@@ -3,32 +3,13 @@ const GameManager = require("./src/gameManager");
 const path = require("path");
 const router = express.Router();
 const cors = require("cors");
-let disableGameCreation = false;
 
 router.get("/create-game", cors(), (req, res) => {
   let minBet = req.query.minBet;
   let startingChips = req.query.startingChips;
 
-  if (!disableGameCreation) {
-    const gameId = GameManager.createGame(minBet, startingChips);
-    res.json({ gameId });
-  } else {
-    res.status(500).json({
-      reason:
-        "New games cannot be created at this moment as the server is under maintenance. " +
-        "We should be back shortly.",
-    });
-  }
-});
-
-router.get("/toggle-game-creation", cors(), (req, res) => {
-  let adminCode = req.query.adminCode;
-
-  if(adminCode === "atxpd") {
-    disableGameCreation = !disableGameCreation;
-  }
-  
-  res.status(200);
+  const gameId = GameManager.createGame(minBet, startingChips);
+  res.json({ gameId });
 });
 
 router.get("*", (req, res) => {
